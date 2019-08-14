2bc38234c987b4b9cf35429dad0ffbc01c131155
tapsmuzira@gmail.com <tapsmuzira@gmail.com>
2019-08-15 00:19:24 +0200

    added simple cardview login screen

---
+++ app/src/main/java/com/mambure/uidesignlab/LoginScreen1.java
@@ -0,0 +1,16 @@
+package com.mambure.uidesignlab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginScreen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_1);
+    }
+
+
+}
