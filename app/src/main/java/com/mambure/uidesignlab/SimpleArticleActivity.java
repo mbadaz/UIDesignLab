eced899f462e93c3c3671758c62913eb7fbaf17b
tapsmuzira@gmail.com <tapsmuzira@gmail.com>
2019-08-19 20:49:04 +0200

    added article detail ui

---
+++ app/src/main/java/com/mambure/uidesignlab/SimpleArticleActivity.java
@@ -0,0 +1,14 @@
+package com.mambure.uidesignlab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimpleArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_article_1);
+    }
+}
